372f17d4f1da1dc544a6e9dba4b908931cdc0a46
钟良发 <zhongliangfa1129@163.com>
2020-03-28 23:15:05 +0800

    编辑资料页的静态布局

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -2,6 +2,14 @@
   <div>
     <!-- 导航页 -->
     <NavigateBar title="编辑资料"/>
+    <!-- 头像 -->
+    <div class="avatar">
+      <img src="https://www.baidu.com/img/bd_logo1.png" alt="">
+    </div>
+    <!-- 按钮列表 -->
+    <Listbar label="昵称" tips="火星网友"/>
+    <Listbar label="密码" tips="******"/>
+    <Listbar label="性别" tips="男"/>
   </div>
 </template>
 
@@ -9,14 +17,27 @@
 <script>
 // 头部导航组件
 import NavigateBar from "@/components/NavigateBar"
+// 导入列表按钮栏的组件
+import Listbar from "@/components/Listbar";
 
 export default {
   components: {
-    NavigateBar
+    NavigateBar,
+    Listbar
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .avatar{
+    display: flex;
+    padding: 20 / 360 * 100vw;
+    justify-content: center;
+    align-items: center;
+    img{
+      width: 100 / 360 * 100vw;
+      height: 100 / 360 * 100vw;
+      border-radius: 50%;
+    }
+  }
 </style>
