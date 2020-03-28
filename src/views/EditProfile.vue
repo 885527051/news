6dcf54793a2c9c900f76be4402b940ade4c22a13
钟良发 <zhongliangfa1129@163.com>
2020-03-29 00:28:27 +0800

    上传图片编辑用户头像成功

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -29,12 +29,16 @@ export default {
   data(){
     return {
       // 用户详情
-      userInfor:{}
+      userInfor:{},
+      // 本地的用户数据
+      userJson: {}
     }
   },
   mounted(){
     // 只要能进入这个页面就表示肯定已经登陆
     const userJson = JSON.parse(localStorage.getItem('userInfo'))
+    // 保存到data，就可以在methods的方法是调用了
+    this.userJson = userJson
     // 请求用户详情
     this.$axios({
       url:"/user/" + userJson.user.id,
@@ -46,13 +50,62 @@ export default {
       const { data } = res.data;
       // 保存到data
       this.userInfor = data
-      console.log(this.userInfor)
+      // console.log(this.userInfor)
     })
   },
   methods:{
-    // 图片上传的方法
+    // 图片上传的方法，file是对文件对象（认为它是不可读的）
     afterRead(file){
       console.log(file)
+      // 创建一个表单对象，上传图片资源必须是表单类型,不能用json
+      // 大家不用去纠结json还是表单的头信息，axios会自动设置的
+      const formData = new FormData()
+
+      // console.log(formData)
+
+      // 通过原有的方法append给表单添加元素
+      // 第一个字符串的file表示接口接收的属性，第二个 file.file是文件对象
+      formData.append('file', file.file)
+      console.log(formData)
+
+
+      // 开始上传
+      this.$axios({
+        url:"/upload",
+        // post请求
+        method:"post",
+        // 添加头信息
+        headers:{
+          Authorization:this.userJson.token
+        },
+        data:formData
+      }).then(res => {
+        console.log(res)
+        // url就是图片的路径
+        const {url} = res.data.data;
+        // 替换掉当前的头像路径
+        this.userInfor.head_img = url
+        // 图片上传成功之后调用编辑用户信息的方法
+        this.handleEdit({
+          head_img:url
+        })
+      })
+    },
+    // 编辑用户信息的函数
+    // data就是请求的参数
+    handleEdit(data){
+      this.$axios({
+        url:"/user_update/"+this.userInfor.id,
+        method:"post",
+        // 添加头信息
+        headers:{
+          Authorization:this.userJson.token
+        },
+        data
+      }).then(res => {
+        console.log(res)
+        this.$toast.success("修改成功")
+      })
     }
   }
 }
@@ -77,6 +130,7 @@ export default {
       left: 50%;
       top: 50%;
       transform: translateX(-50 / 360 * 100vw) translateY(-50 / 360 * 100vw);
+      opacity: 0;
     }
   }
 </style>
