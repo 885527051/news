e422a4df1929fe234f9a7919c487f2937922c81b
钟良发 <zhongliangfa1129@163.com>
2020-03-31 00:43:14 +0800

    我的跟帖数据的请求和渲染

--- src/views/Comment.vue
+++ src/views/Comment.vue
@@ -5,21 +5,22 @@
     <NavigateBar title="我的跟贴" />
 
     <!-- 要循环的结构 -->
-    <div class="comment-item">
-      <div class="date">2019-2020 20:20</div>
+    <div class="comment-item" v-for="(item,index) in comments" :key="index">
+      <div class="date">{{ moment(item.create_date).format("YYYY-MM-DD hh:mm")}}</div>
       <!-- 有回复的评论需要展示的, 就是我回复了谁 -->
-      <div class="parent">
-        <div class="parent-user">回复：火星网友</div>
+      <div class="parent" v-if="item.parent">
+        <div class="parent-user">回复：{{item.parent.user.nickname}}</div>
         <div class="parent-conter">
-          有回复的评论需要展示的, 就是我回复了谁有回复的评论需要展示的, 就是我回复了谁
+          {{item.parent.content}}
         </div>
       </div>
       <!-- 自己发布的内容 -->
       <div class="comment-content">
-        自己发布的内容自己发布的内容自己发布的内容自己发布的内容自己发布的内容自己发布的内容自己发布的内容自己发布的内容
+        {{item.content}}
       </div>
       <router-link to="#" class="link-post">
-        原文：发布评论的新闻文章的标题
+        <div>原文：{{item.post.title}}</div>
+        <span class="iconfont iconjiantou1"></span>
       </router-link>
     </div>
   </div>
@@ -35,6 +36,33 @@ export default {
   // 注册组件
   components:{
     NavigateBar
+  },
+  data(){
+    return {
+      // 评论列表
+      comments:[],
+      // 日期处理的工具库
+      moment,
+      localUserJson:[]
+    }
+  },
+  mounted(){
+    // 本地数据
+    const localUserJson  = JSON.parse(localStorage.getItem('userInfo'))
+    this.localUserJson = localUserJson
+    // 请求评论列表
+    this.$axios({
+      url:"/user_comments",
+      headers:{
+        Authorization : this.localUserJson.token
+      }
+    }).then(res => {
+      console.log(res);
+      
+      const {data} = res.data
+      // 把评论列表保存到data
+      this.comments = data
+    })
   }
 
 }
@@ -56,7 +84,22 @@ export default {
       margin: 15/ 360 * 100vw;
     }
     .link-post{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      position: relative;
+      padding-right: 30px;
       color: #999;
+      div{
+        flex: 1;
+        // 文字超出出现省略号一定是块元素才有作用
+        overflow: hidden;
+        text-overflow:ellipsis;
+        white-space: nowrap;
+      }
+        span{
+          margin-left: 5px;
+        }
     }
   }
 </style>
