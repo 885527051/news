cb67d03d0d65ddc28399fc9455b1abde7cbc3b7c
钟良发 <zhongliangfa1129@163.com>
2020-04-02 16:12:55 +0800

    分页组件的使用

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -13,76 +13,109 @@
       </router-link>
     </div>
     <!-- tab栏切换 -->
-      <!-- v-model：就是当前的索引值，是唯一的，比较类似于for循环的key -->
-      <!-- sticky：是否使用粘性定位布局 -->
-      <!-- swipeable: 是否开启手势滑动切换 -->
-      <van-tabs v-model="active" sticky swipeable>
-        <van-tab v-for="(item, index) in categories" :key="index" :title="item">
-          <div>
-            <!-- 文章列表的组件 -->
+    <!-- v-model：就是当前的索引值，是唯一的，比较类似于for循环的key -->
+    <!-- sticky：是否使用粘性定位布局 -->
+    <!-- swipeable: 是否开启手势滑动切换 -->
+    <van-tabs v-model="active" sticky swipeable>
+      <van-tab v-for="(item, index) in categories" :key="index" :title="item">
+        <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+          <van-cell v-for="item in list" :key="item">
             <!-- 只有单张图片的 -->
-            <PostItem1/>
-            <!-- 大于等于3张图片的组件 -->
-            <PostItem2/>
-            <!-- <p v-for="index in 200" :key="index">内容 {{ index }}</p> -->
-        </div>
-        </van-tab>
-      </van-tabs>
-
+            <PostItem1 />
+          </van-cell>
+        </van-list>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
 // 文章列表的组件,只有单张图片的
-import PostItem1 from "@/components/PostItem1"
+import PostItem1 from "@/components/PostItem1";
 // 大于等于3张图片的组件
-import PostItem2 from "@/components/PostItem2"
+import PostItem2 from "@/components/PostItem2";
 export default {
-  components:{
+  components: {
     PostItem1,
     PostItem2
   },
-  data(){
+  data() {
     return {
-      categories:[ '关注','娱乐','体育','汽车','房产','关注',
-            '关注','娱乐','体育','汽车','房产','关注', "∨" ],
+      categories: [
+        "关注",
+        "娱乐",
+        "体育",
+        "汽车",
+        "房产",
+        "关注",
+        "关注",
+        "娱乐",
+        "体育",
+        "汽车",
+        "房产",
+        "关注",
+        "∨"
+      ],
       // 记录当前tab的切换的索引
-      active:0,
+      active: 0,
+      // 假设这个数组是后台返回的数据
+      list: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 10个1
+      loading: false, // 是否正在加载中
+      finished: false, // 是否已经加载完毕
       // 监听属性
-      watch:{
+      watch: {
         // 监听tab栏的切换
-        active(){
+        active() {
           // 判断如果点击的是最后一个图标，跳转到栏目管理页
-          if(this.active === this.categories.length -1){
-            this.$router.push("/栏目管理")
+          if (this.active === this.categories.length - 1) {
+            this.$router.push("/栏目管理");
           }
         }
       }
+    };
+  },
+  methods: {
+    onLoad() {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1);
+        }
+
+        // 加载状态结束
+        this.loading = false;
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true;
+        }
+      }, 5000);
     }
   }
-}
+};
 </script>
 
 <style scoped lang="less">
-.header{
-  height: 50/360*100vw;
+.header {
+  height: 50/360 * 100vw;
   background: #ff0000;
   display: flex;
-  padding: 0 20/360*100vw;
+  padding: 0 20/360 * 100vw;
   justify-content: space-between;
   align-items: center;
   color: #fff;
-  .iconnew{
+  .iconnew {
     font-size: 20px;
     // 定义 2D 缩放转换
     transform: scale(3);
     position: relative;
-    left: 20/360*100vw;
+    left: 20/360 * 100vw;
   }
-  .search{
+  .search {
     flex: 1;
-    height: 32/360*100vw;
-    margin: 0 50/360*100vw;
+    height: 32/360 * 100vw;
+    margin: 0 50/360 * 100vw;
     background: rgba(255, 255, 255, 0.5);
     display: flex;
     justify-content: center;
@@ -92,11 +125,11 @@ export default {
     line-height: 1;
     // 设置垂直对齐方式，middle=放在父元素中部
     vertical-align: middle;
-    span{
+    span {
       margin-right: 5px;
     }
   }
-  .iconnew{
+  .iconnew {
     font-size: 20px;
   }
 }
@@ -104,34 +137,33 @@ export default {
 // 重置vant组件的样式
 // 如果在scoped声明中去修改第三方库的class样式，必须要在前面添加/deep/
 
-/deep/ .van-tabs__nav{
+/deep/ .van-tabs__nav {
   background: #eee;
   // 默认值，没有定位
   position: static;
 }
-/deep/ .van-tab{
-  flex-basis: 15%!important;
+/deep/ .van-tab {
+  flex-basis: 15% !important;
 }
 
-/deep/ .van-tab:nth-last-child(2){
+/deep/ .van-tab:nth-last-child(2) {
   background: red;
-  width: 20px!important;
+  width: 20px !important;
   position: absolute;
   right: 0;
   top: 0;
-  // 不设置 box-sizing: border-box; C3盒子模型  
+  // 不设置 box-sizing: border-box; C3盒子模型
   box-sizing: unset;
 }
 
-/deep/ .van-tabs__wrap{
+/deep/ .van-tabs__wrap {
   padding-right: 20px;
 }
 
-/deep/ .van-tabs__line{
+/deep/ .van-tabs__line {
   display: none;
 }
-/deep/ .van-tab--active{
+/deep/ .van-tab--active {
   border-bottom: 2px red solid;
 }
-
 </style>
