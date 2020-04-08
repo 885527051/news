80c4134d2cc2c515215ccee0589adb35bb9bd4d2
钟良发 <zhongliangfa1129@163.com>
2020-04-08 19:59:14 +0800

    keep-alive缓存首页

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
-    <!-- <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <button @click="$toast('弹窗测试')">测试弹窗</button>  -->
-    <!-- 匹配主键的坑 -->
+    <!-- keep-alive会缓存组件，如果不加include会缓存所有的组件，
+        <router-view />	        但是include需要指定缓存哪个页面组件，取的是页面组件的name值 -->
+    <keep-alive include="index">
+        <!-- 匹配主键的坑 -->
     <router-view/>
+    </keep-alive>
   </div>
 </template>
 
