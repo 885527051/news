abd886ae74383b868be1694ebb3e6b1d38b3f875
钟良发 <zhongliangfa1129@163.com>
2020-04-08 15:24:13 +0800

    点赞和取消点赞

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -8,27 +8,26 @@
           <span class="iconfont iconnew"></span>
         </div>
         <!-- 如果关注是false，就加上active这个class，显示一个红色按钮 -->
-        <span class="follow"
-         :class="post.has_follow ? '' : 'active'"
-         @click="handleFollow"
-         >
-         {{post.has_follow ? '已关注' : '关注'}}
-         </span>
+        <span
+          class="follow"
+          :class="post.has_follow ? '' : 'active'"
+          @click="handleFollow"
+        >{{post.has_follow ? '已关注' : '关注'}}</span>
       </div>
       <!-- 文章标题 -->
       <h2 class="title">{{post.title}}</h2>
       <!-- 文章的作者 -->
-      <p class="author">{{post.user.nickname}} {{moment(post.create_date).format(`YYYY-MM-DD hh:mm:ss`)}}</p>
+      <p
+        class="author"
+      >{{post.user.nickname}} {{moment(post.create_date).format(`YYYY-MM-DD hh:mm:ss`)}}</p>
       <!-- 文章的详情 -->
-      <div
-        class="content" v-html="post.content"
-      ></div>
+      <div class="content" v-html="post.content"></div>
       <!-- 按钮列表 -->
       <div class="actions">
-        <div class="actions-item">
+        <div class="actions-item" @click="handleLike">
           <span class="iconfont icondianzan"></span>
           <!-- 点赞 -->
-          <i>{{Number(post.has_like)}}</i>
+          <i>{{Number(post.like_length)}}</i>
         </div>
         <div class="actions-item">
           <span class="iconfont iconweixin"></span>
@@ -64,65 +63,85 @@
 import moment from "moment";
 
 export default {
-  data(){
+  data() {
     return {
       // 文章的详情
-      post:{
+      post: {
         // 为什么要加这个默认值，因为模板需要访问post.user.nickname
-        user:{}
+        user: {}
       },
       moment,
-      token:""
-    }
+      token: ""
+    };
   },
-  mounted(){
+  mounted() {
     // 获取文章的id
-    const {id} = this.$route.params;
+    const { id } = this.$route.params;
     // 本地的token
-    const {token} = JSON.parse(localStorage.getItem("userInfo")) || {}
+    const { token } = JSON.parse(localStorage.getItem("userInfo")) || {};
     // 保存一份到data
-    this.token = token
+    this.token = token;
     const config = {
-      url:"/post/"+id
-    }
+      url: "/post/" + id
+    };
     // 如果token有值给头信息加上token
-    if(token){
-      config.headers = { Authorization: token }
+    if (token) {
+      config.headers = { Authorization: token };
     }
     // 请求文章的详情
-    this.$axios(config).then(res=>{
+    this.$axios(config).then(res => {
       // data是文章的详情
-      const {data} = res.data;
+      const { data } = res.data;
       this.post = data;
-    })
+    });
   },
-  methods:{
+  methods: {
     // 关注和取消关注
-    handleFollow(){
-      let url = ""
+    handleFollow() {
+      let url = "";
       // 先判断当前的状态如果是关注，就取消关注
-      if(this.post.has_follow){
+      if (this.post.has_follow) {
         // 取消关注
-        url = "/user_unfollow/" + this.post.user.id
-      }else{
+        url = "/user_unfollow/" + this.post.user.id;
+      } else {
         // 关注
-        url = "/user_follows/" + this.post.user.id
+        url = "/user_follows/" + this.post.user.id;
       }
 
       this.$axios({
         url,
-        headers:{
+        headers: {
           Authorization: this.token
         }
       }).then(res => {
         // console.log(res);
-        const {data} = res
+        const { data } = res;
         // console.log(data);
-        
-
         // 关注成功之后修改关注状态
         this.post.has_follow = !this.post.has_follow;
-        this.$toast.success(data.message)
+        this.$toast.success(data.message);
+      });
+    },
+    // 文章点赞
+    handleLike() {
+      this.$axios({
+        url: "/post_like/" + this.post.id,
+        headers: {
+          Authorization: this.token
+        }
+      }).then(res => {
+        // 修改点赞的状态
+        this.post.has_like = !this.post.has_like;
+        // 判断当前是否点赞
+        if(this.post.has_like){
+          // 如果是点赞就加1
+          this.post.like_length += 1
+        }else{
+          // 取消点赞就减1
+          this.post.like_length -= 1
+        }
+        // 弹窗提示
+        this.$toast.success(res.data.message)
       })
     }
   }
@@ -132,7 +151,7 @@ export default {
 <style scoped lang="less">
 .main {
   padding: 20/360 * 100vw;
-  padding-bottom: 100/360*100vw;
+  padding-bottom: 100/360 * 100vw;
   border-bottom: 5px #eee solid;
 
   .herder {
@@ -156,7 +175,7 @@ export default {
       border-radius: 50px;
       font-size: 12px;
     }
-    .active{
+    .active {
       background: red;
       border-color: red;
       color: #fff;
@@ -175,7 +194,7 @@ export default {
     line-height: 1.8;
     margin-top: 10/360 * 100vw;
     // 不是本页面的标签或者类型，都可以通过/deep/来访问
-    /deep/ img{
+    /deep/ img {
       max-width: 100%;
     }
   }
@@ -200,34 +219,34 @@ export default {
     }
   }
 }
-.footer{
+.footer {
   position: fixed;
   width: 100%;
   bottom: 0;
   left: 0;
   background: #fff;
-  padding: 10/360*100vw 20/360*100vw;
+  padding: 10/360 * 100vw 20/360 * 100vw;
   display: flex;
   justify-content: space-between;
   align-items: center;
   box-sizing: border-box;
 
-  .comment-input{
+  .comment-input {
     flex: 1;
-    margin-right: 20/360*100vw;
-    height: 30/360*100vw;
-    padding: 0 20/360*100vw;
-    line-height: 30/360*100vw;
+    margin-right: 20/360 * 100vw;
+    height: 30/360 * 100vw;
+    padding: 0 20/360 * 100vw;
+    line-height: 30/360 * 100vw;
     border-radius: 50px;
     background: #eee;
   }
-  .icons{
+  .icons {
     position: relative;
-    margin: 0 8/360*100vw;
-    .iconfont{
+    margin: 0 8/360 * 100vw;
+    .iconfont {
       font-size: 23px;
     }
-    i{
+    i {
       position: absolute;
       right: -10px;
       top: -5px;
@@ -239,7 +258,7 @@ export default {
       line-height: 1;
       border-radius: 50px;
     }
-    .active{
+    .active {
       color: red;
     }
   }
